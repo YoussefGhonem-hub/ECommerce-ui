b3520155607ca2f4a5cc07ada5ec6a97dc928a8d
Youssef Ghonem <youssef.fcih@gmail.com>
2025-12-13 10:24:00 +0200

    fixes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.ecommerce.app',
-  appName: 'Ecommerce App',
+  appName: 'API-Brand',
   webDir: 'dist/vuexy',
   server: {
     androidScheme: 'http',
